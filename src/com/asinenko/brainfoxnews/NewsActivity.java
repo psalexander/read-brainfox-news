004d6fcd6101450b1fa29501643a8c7c4998ac3a
Alexander Asinenko <asinenko.alexander@navitel.su>
2014-01-27 18:49:38 +0400

    Редактирую новостное активити

--- src/com/asinenko/brainfoxnews/NewsActivity.java
+++ src/com/asinenko/brainfoxnews/NewsActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.res.Configuration;
 import android.view.Menu;
-import android.widget.Gallery;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class NewsActivity extends Activity {
@@ -13,6 +13,7 @@ public class NewsActivity extends Activity {
 	private TextView shortTextView;
 	private TextView mainTextView;
 	private TextView dateTextView;
+	private LinearLayout imageLayout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +25,22 @@ public class NewsActivity extends Activity {
 		mainTextView = (TextView)findViewById(R.id.mainTextView);
 		dateTextView = (TextView)findViewById(R.id.dateTextView);
 
+		imageLayout = (LinearLayout)findViewById(R.id.imageLayout);
+	//	imageLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,200));
+
 		titleTextView.setText(getIntent().getExtras().getString("title"));
 		shortTextView.setText(getIntent().getExtras().getString("short"));
-		mainTextView.setText(getIntent().getExtras().getString("id"));
+		//mainTextView.setText(getIntent().getExtras().getString("id"));
+		mainTextView.setText("sdgdfgdfgdfgsdfgdsfgрапрапрапррsdfg\nsdgdfgdfgdfgsdfgdsfgsdfg\nsdgdfgdfgdfgsdfпарапрапрgdsfgsdfg\nsdgdfgdfgdfgsdfгшнгшнгшнгшgdsfgsdfg\nsdgdfgdfgdfgsdfgdsfgsdfg\n");
 		dateTextView.setText(getIntent().getExtras().getString("date"));
+		
+//		ImageView im = new ImageView(this);
+//		im.setImageResource(R.drawable.marussia);
+//		imageLayout.addView(im);
+//		
+//		ImageView im2 = new ImageView(this);
+//		im2.setImageResource(R.drawable.marcell_sebestyen);
+//		imageLayout.addView(im2);
 	}
 
 	@Override
