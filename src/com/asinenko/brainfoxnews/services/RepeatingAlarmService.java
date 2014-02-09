6a6e9f72846f4e769bbff7cb89940a26ece7b07d
alexander <alexander@asinenko.com>
2014-02-09 22:48:15 +0400

    начал писать сервис

---
+++ src/com/asinenko/brainfoxnews/services/RepeatingAlarmService.java
@@ -0,0 +1,20 @@
+package com.asinenko.brainfoxnews.services;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RepeatingAlarmService extends BroadcastReceiver {
+
+	private GPSLocationDataSource gpsLocationDataSource;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+    	
+        Toast.makeText(context, "It's Service Time!", Toast.LENGTH_LONG).show();
+        Log.v(this.getClass().getName(), "Timed alarm onReceive() started at time: " + new java.sql.Timestamp(System.currentTimeMillis()).toString());
+    }
+}
