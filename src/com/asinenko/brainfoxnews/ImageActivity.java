c2135903d6a3548b809ce8383ce6ab195d20c368
Alexander Asinenko <asinenko.alexander@navitel.su>
2014-02-10 15:57:48 +0400

    Добавил открытие картинки

---
+++ src/com/asinenko/brainfoxnews/ImageActivity.java
@@ -0,0 +1,104 @@
+package com.asinenko.brainfoxnews;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.asinenko.brainfoxnews.items.JsonParser;
+import com.asinenko.brainfoxnews.items.NewsListItem;
+import com.asinenko.brainfoxnews.items.NewsSQLItem;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class ImageActivity extends Activity {
+
+	private ImageView imageView;
+	private String imageUrl;
+	private ProgressBar progressBar;
+	private static final String IMAGE_URL = "http://baklikov.ru/ttt.php?action=image&id=";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.activity_image);
+
+		imageView = (ImageView)findViewById(R.id.oneImageView);
+		progressBar = (ProgressBar)findViewById(R.id.progressBar);
+
+		imageUrl = getIntent().getExtras().getString("url");
+
+		new RequestTask().execute(IMAGE_URL + imageUrl);
+	}
+
+	private Bitmap image = null;
+
+	class RequestTask extends AsyncTask<String, String, String>{
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			progressBar.setVisibility(ProgressBar.VISIBLE);
+		}
+
+		@Override
+		protected String doInBackground(String... uri) {
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpResponse response;
+			try {
+				response = httpclient.execute(new HttpGet(uri[0]));
+				StatusLine statusLine = response.getStatusLine();
+				if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+					
+					
+					//ByteArrayOutputStream out = new ByteArrayOutputStream();
+					//response.getEntity().writeTo(out);
+					//out.close();
+					image = BitmapFactory.decodeStream(response.getEntity().getContent());
+					//responseString = out.toString();
+				} else{
+					//Closes the connection.
+					response.getEntity().getContent().close();
+					throw new IOException(statusLine.getReasonPhrase());
+				}
+			} catch (ClientProtocolException e) {
+				//TODO Handle problems
+			} catch (IOException e) {
+				//TODO Handle problems
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			if(image != null)
+				imageView.setImageBitmap(image);
+			progressBar.setVisibility(ProgressBar.INVISIBLE);
+		}
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+	}
+
+}
