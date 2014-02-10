5f33ebda849d1c70e92269d1654e8e4e8c487282
Alexander Asinenko <asinenko.alexander@navitel.su>
2014-02-10 17:47:13 +0400

    Выделил все стандартные урлы в один файл

---
+++ src/com/asinenko/brainfoxnews/Urls.java
@@ -0,0 +1,7 @@
+package com.asinenko.brainfoxnews;
+
+public class Urls {
+	public static String URL_GET_NEWS_LIST = "http://baklikov.ru/ttt.php?action=list&ts=";
+	public static String URL_GET_NEWS_ITEM = "http://baklikov.ru/ttt.php?action=details&id=";
+	public static String URL_GET_IMAGE = "http://baklikov.ru/ttt.php?action=image&id=";
+}
