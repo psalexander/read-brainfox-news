a5075399540466345219589ccfc4cd225735e856
Alexander Asinenko <asinenko.alexander@navitel.su>
2014-02-10 17:48:16 +0400

    Сделал логику отображения картинок для одной картинки, списка картинок и ситуации без картинок.

--- src/com/asinenko/brainfoxnews/activity/NewsActivity.java
+++ src/com/asinenko/brainfoxnews/activity/NewsActivity.java
@@ -14,9 +14,9 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import com.asinenko.brainfoxnews.HorizontalListView;
-import com.asinenko.brainfoxnews.ImageActivity;
 import com.asinenko.brainfoxnews.LazyAdapter;
 import com.asinenko.brainfoxnews.R;
+import com.asinenko.brainfoxnews.Urls;
 import com.asinenko.brainfoxnews.items.JsonParser;
 import com.asinenko.brainfoxnews.items.NewsItem;
 
@@ -26,19 +26,23 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.util.Log;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.Toast;
 
 public class NewsActivity extends Activity {
 
@@ -50,6 +54,7 @@ public class NewsActivity extends Activity {
 	private TextView mainTextView;
 	private TextView dateTextView;
 	private ProgressBar progressBar;
+	private ImageView imageView;
 
 	private Activity activity;
 
@@ -59,7 +64,11 @@ public class NewsActivity extends Activity {
 	private String mainText;
 	private String date;
 
+	private Bitmap image;
 	private NewsItem newsItem;
+
+	private int displayWidth;
+	private int displayHeigth;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +77,7 @@ public class NewsActivity extends Activity {
 		activity = this;
 
 		listView = (HorizontalListView) findViewById(R.id.imageListView);
+		imageView = (ImageView) findViewById(R.id.oneImageNewsView);
 
 		titleTextView = (TextView)findViewById(R.id.titleTextView);
 		shortTextView = (TextView)findViewById(R.id.shortTextView);
@@ -75,6 +85,13 @@ public class NewsActivity extends Activity {
 		dateTextView = (TextView)findViewById(R.id.dateTextView);
 		progressBar = (ProgressBar)findViewById(R.id.progressBar);
 		progressBar.setVisibility(ProgressBar.INVISIBLE);
+
+
+		Display display =  ((WindowManager)getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+		Point size = new Point();
+		display.getSize(size);
+		displayWidth = size.x;
+		displayHeigth = size.y;
 
 		id = getIntent().getExtras().getString("newsid");
 		title = getIntent().getExtras().getString("title");
@@ -125,23 +142,42 @@ public class NewsActivity extends Activity {
 			mainTextView.setText(newsItem.getText());
 			dateTextView.setText(newsItem.getDate());
 
-			List<String> list = new ArrayList<String>();
-			for(int i = 0; i < newsItem.getImages().size(); i++){
-				String id = newsItem.getImages().get(i);
-				String url = IMAGE_URL + id + "&h=300";
-				list.add(url);
-			}
-			String[] array = list.toArray(new String[list.size()]);
-			final LazyAdapter ladapter=new LazyAdapter(activity, array);
-			listView.setAdapter(ladapter);
-			listView.setOnItemClickListener(new OnItemClickListener() {
-				@Override
-				public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
-					Intent intent = new Intent(NewsActivity.this, ImageActivity.class);
-					intent.putExtra("url", newsItem.getImages().get(position));
-					startActivity(intent);
+			if(newsItem.getImages().size() == 0){
+				listView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,0));
+			}else if(newsItem.getImages().size() == 1){
+				listView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,0));
+				int width = displayWidth;
+				if(displayHeigth < displayWidth)
+					width = displayHeigth;
+				imageView.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Intent intent = new Intent(NewsActivity.this, ImageActivity.class);
+						intent.putExtra("url", newsItem.getImages().get(0));
+						startActivity(intent);
+					}
+				});
+				new RequestOneImageTask().execute(Urls.URL_GET_IMAGE + newsItem.getImages().get(0) + "&w=" + String.valueOf(width));
+			}else{
+				listView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,300));
+				List<String> list = new ArrayList<String>();
+				for(int i = 0; i < newsItem.getImages().size(); i++){
+					String id = newsItem.getImages().get(i);
+					String url = IMAGE_URL + id + "&h=300";
+					list.add(url);
 				}
-			});
+				String[] array = list.toArray(new String[list.size()]);
+				final LazyAdapter ladapter=new LazyAdapter(activity, array);
+				listView.setAdapter(ladapter);
+				listView.setOnItemClickListener(new OnItemClickListener() {
+					@Override
+					public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+						Intent intent = new Intent(NewsActivity.this, ImageActivity.class);
+						intent.putExtra("url", newsItem.getImages().get(position));
+						startActivity(intent);
+					}
+				});
+			}
 			progressBar.setVisibility(ProgressBar.INVISIBLE);
 		}
 	}
@@ -189,6 +225,46 @@ public class NewsActivity extends Activity {
 			RelativeLayout lay = (RelativeLayout)vi.findViewById(R.id.imageRelativeLayout);
 			lay.addView(data.get(position));
 			return vi;
+		}
+	}
+
+	class RequestOneImageTask extends AsyncTask<String, String, String>{
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			progressBar.setVisibility(ProgressBar.VISIBLE);
+		}
+
+		@Override
+		protected String doInBackground(String... uri) {
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpResponse response;
+			try {
+				response = httpclient.execute(new HttpGet(uri[0]));
+				StatusLine statusLine = response.getStatusLine();
+				if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+					image = BitmapFactory.decodeStream(response.getEntity().getContent());
+				} else{
+					response.getEntity().getContent().close();
+					throw new IOException(statusLine.getReasonPhrase());
+				}
+			} catch (ClientProtocolException e) {
+				//TODO Handle problems
+			} catch (IOException e) {
+				//TODO Handle problems
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			imageView.setVisibility(ImageView.VISIBLE);
+			if(image != null){
+				imageView.setImageBitmap(image);
+				imageView.invalidate();
+			}
+			progressBar.setVisibility(ProgressBar.INVISIBLE);
 		}
 	}
 }
