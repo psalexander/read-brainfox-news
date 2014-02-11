e055adecf56c8d1f14e00abac56cfec78a90480c
Alexander Asinenko <asinenko.alexander@navitel.su>
2014-02-11 12:58:36 +0400

    добавил урл

--- src/com/asinenko/brainfoxnews/Urls.java
+++ src/com/asinenko/brainfoxnews/Urls.java
@@ -1,6 +1,7 @@
 package com.asinenko.brainfoxnews;
 
 public class Urls {
+	public static final String URL_GET_NEWS_COUNT = "http://baklikov.ru/ttt.php?action=count=&ts=";
 	public static final String URL_GET_NEWS_LIST = "http://baklikov.ru/ttt.php?action=list&ts=";
 	public static final String URL_GET_NEWS_ITEM = "http://baklikov.ru/ttt.php?action=details&id=";
 	public static final String URL_GET_IMAGE = "http://baklikov.ru/ttt.php?action=image&id=";
