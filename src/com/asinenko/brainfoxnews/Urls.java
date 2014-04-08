64dc56fc05b9b22aeb6e66026abb7eb03d5a5c14
Alexander Asinenko <asinenko.alexander@navitel.su>
2014-04-08 17:55:40 +0400

    перенос на другой сервер

--- src/com/asinenko/brainfoxnews/Urls.java
+++ src/com/asinenko/brainfoxnews/Urls.java
@@ -1,8 +1,13 @@
 package com.asinenko.brainfoxnews;
 
 public class Urls {
-	public static final String URL_GET_NEWS_COUNT = "http://baklikov.ru/ttt.php?action=count=&ts=";
-	public static final String URL_GET_NEWS_LIST = "http://baklikov.ru/ttt.php?action=list&ts=";
-	public static final String URL_GET_NEWS_ITEM = "http://baklikov.ru/ttt.php?action=details&id=";
-	public static final String URL_GET_IMAGE = "http://baklikov.ru/ttt.php?action=image&id=";
+//	public static final String URL_GET_NEWS_COUNT = "http://baklikov.ru/ttt.php?action=count=&ts=";
+//	public static final String URL_GET_NEWS_LIST = "http://baklikov.ru/ttt.php?action=list&ts=";
+//	public static final String URL_GET_NEWS_ITEM = "http://baklikov.ru/ttt.php?action=details&id=";
+//	public static final String URL_GET_IMAGE = "http://baklikov.ru/ttt.php?action=image&id=";
+
+	public static final String URL_GET_NEWS_COUNT = "http://brain-fox.ru/ttt.php?action=count=&ts=";
+	public static final String URL_GET_NEWS_LIST = "http://brain-fox.ru/ttt.php?action=list&ts=";
+	public static final String URL_GET_NEWS_ITEM = "http://brain-fox.ru/ttt.php?action=details&id=";
+	public static final String URL_GET_IMAGE = "http://brain-fox.ru/ttt.php?action=image&id=";
 }
